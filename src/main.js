f6cac159a162c16976201d694bfc63e694271a96
alpachm <alexpacheco407@gmail.com>
2022-12-31 14:33:52 -0500

    miller

---
+++ src/main.js
@@ -0,0 +1,7 @@
+let icon = document.getElementById('icon_menu');
+let menu = document.getElementById('header_menu')
+
+icon.addEventListener('click', function(){
+    menu.classList.toggle('show__menu')
+})
+
